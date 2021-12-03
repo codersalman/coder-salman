9f6f294949c7fd400a427b98de4fb89172b8c262
Salman Shaikh <83803180+codersalman@users.noreply.github.com>
2021-12-03 10:01:05 +0530

    Config Update

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,11 +7,13 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteTitle: `Codersalman`,
+    siteTitleAlt: `Codersalman - Dev Portfollio`,
+    siteHeadline: `Codersalman - Dev Portfollio`,
+    siteUrl: `https://codersalman.gatsbyjs.io/`,
+    siteDescription: `Mobile & Web application Developer,
+an #opensource contributer and #tech content creator.
+I solve surrounding problems with Code & Technologies.`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
     author: `@lekoarts_de`,
